210d6de665c01f91e0c5648a1bc5dcb9d41d521f
Craig Thacker <craigthackerx@gmail.com>
2022-06-02 16:02:55 +0100

    update

---
+++ gui-tooling-container/container/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+start_xrdp_services() {
+    # Preventing xrdp startup failure
+    rm -rf /var/run/xrdp-sesman.pid
+    rm -rf /var/run/xrdp.pid
+    rm -rf /var/run/xrdp/xrdp-sesman.pid
+    rm -rf /var/run/xrdp/xrdp.pid
+
+    # Use exec ... to forward SIGNAL to child processes
+    xrdp-sesman && exec xrdp -n
+}
+
+stop_xrdp_services() {
+    xrdp --kill
+    xrdp-sesman --kill
+    exit 0
+}
+
+echo Entryponit script is Running...
+
+echo -e "starting xrdp services...\n"
+
+trap "stop_xrdp_services" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+start_xrdp_services
